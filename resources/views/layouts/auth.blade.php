8363d41a7bdcb8419c34892bc9f68ae46a4f1e7b
unknown <Fais@PC.sacbuk.org>
2022-12-26 17:15:54 +0000

    Laravel-Checklist Role and Permission

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Option 1: CoreUI for Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/@coreui/coreui@4.2.0/dist/css/coreui.min.css" rel="stylesheet">
+
+
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+</head>
+
+<body>
+    <div class="bg-light min-vh-100 d-flex flex-row align-items-center">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <div class="card-group d-block d-md-flex row">
+                        <div class="card col-md-7 p-4 mb-0">
+                            <div class="card-body">
+                                @yield('content')
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    <!-- Option 3: Separate Popper and CoreUI/CoreUI PRO  for Bootstrap JS -->
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@coreui/coreui@4.2.0/dist/js/coreui.min.js"></script>
+
+</body>
+
+</html>
---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,51 @@
+<div class="sidebar-brand d-none d-md-flex">
+    <svg class="sidebar-brand-full" width="118" height="46" alt="CoreUI Logo">
+        <use xlink:href="{{ asset('assets/brand/coreui.svg#full') }}"></use>
+    </svg>
+    <svg class="sidebar-brand-narrow" width="46" height="46" alt="CoreUI Logo">
+        <use xlink:href="{{ asset('assets/brand/coreui.svg#signet') }}"></use>
+    </svg>
+</div>
+<ul class="sidebar-nav" data-coreui="navigation" data-simplebar="">
+    <li class="nav-item"><a class="nav-link" href="{{ route('home') }}">
+            <svg class="nav-icon">
+                <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-speedometer') }}"></use>
+            </svg> Dashboard</a></li>
+    @if (auth()->user()->is_admin)
+
+
+
+    <li class="nav-title">Admin</li>
+    <li class="nav-group"><a class="nav-link " href="{{ route('admin.pages.index') }}">
+            <svg class="nav-icon">
+                <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-puzzle') }}"></use>
+            </svg> Pages
+        </a>
+    </li>
+
+    <li class="nav-group"><a class="nav-link nav-group-toggle" href="#">
+            <svg class="nav-icon">
+                <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-puzzle') }}"></use>
+            </svg> Base</a>
+        <ul class="nav-group-items">
+            <li class="nav-item"><a class="nav-link" href="base/accordion.html"><span class="nav-icon"></span>
+                    Accordion</a></li>
+
+        </ul>
+    </li>
+
+    @endif
+
+    <li class="nav-item"><a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+            <svg class="nav-icon">
+                <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-account-logout') }}"></use>
+            </svg> {{ __('Logout') }}
+        </a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            @csrf
+        </form>
+    </li>
+
+</ul>
+<button class="sidebar-toggler" type="button" data-coreui-toggle="unfoldable"></button>
